1d7902f02cad06702f9a83e78ce92ce2e169089d
1801a <45133116+1801a@users.noreply.github.com>
2018-11-22 05:31:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "&"// البرفكس هنا
+
 client.on('ready', () => {
-  console.log('bot online ');
-});
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('By Wenteed')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 
-
+  
+ 
 
 client.on("message", message => {
 	var prefix = "-";
@@ -447,4 +470,5 @@ client.on("message", message => {
 
   
   
-client.login(process.env.TOKEN);
+
+client.login(process.env.BOT_TOKEN);
